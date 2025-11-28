77271d4f28e67945ee9b012665a4a86fb893748e
Adnene Tahri <tahrigarroug@gmail.com>
2025-11-28 10:09:14 +0100

    vs code1

--- lib/domain/usecases/health/monitor_health_usecase.dart
+++ lib/domain/usecases/health/monitor_health_usecase.dart
@@ -1,16 +1,22 @@
 import 'package:either_dart/either.dart';
 
+import '../../../core/errors/exceptions.dart';
+import '../../../core/errors/failures.dart';
 import '../../repositories/health_repository.dart';
 import '../base_usecase.dart';
 
-class MonitorHealthUseCase extends UseCase<void, String> {
+class MonitorHealthUseCase extends UseCase<bool, String> {
   final HealthRepository repository;
 
   MonitorHealthUseCase(this.repository);
 
   @override
-  Future<UseCaseResult<void>> execute(String siteId) async {
+  Future<UseCaseResult<bool>> execute(String siteId) async {
     final result = await repository.monitorSiteUptime(siteId);
-    return result.map((_) => null);
+    return result.fold(
+      (failure) =>
+          UseCaseResult.error(UseCaseException(message: failure.message, code: 'HEALTH_MONITOR_FAILURE')),
+      (isUp) => UseCaseResult.success(isUp),
+    );
   }
 }
