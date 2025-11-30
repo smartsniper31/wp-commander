58caf96b880151ed19e254352ecc30895311df79
Adnene Tahri <tahrigarroug@gmail.com>
2025-11-30 08:06:37 +0100

    vs code 4

---
+++ settings_page.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class SettingsPage extends StatelessWidget {
+  const SettingsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Paramètres'),
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back),
+          onPressed: () => Navigator.of(context).pop(),
+        ),
+      ),
+      body: const Center(
+        child: Text('Page des paramètres'),
+      ),
+    );
+  }
+}
